52db7504821df63ed59e600887cae64ef7939474
Robin Lovelace <rob00x@gmail.com>
2020-05-01 08:49:44 +0100

    Add working package

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Elevation in central Lisbon, Portugal
+#'
+#' A dataset containing elevation in and around Lisbon with a geographic resolution of 10m.
+#'
+#' @format A raster dataset containing elevation above sea level in a 1km bounding box in Lisbon, Portugal.
+#' @source \url{https://github.com/rspatial/terra/issues/29}
+#' @examples
+#' names(dem_lisbon_raster)
+#' raster::plot(dem_lisbon_raster)
+#' plot(lisbon_road_segments["Avg_Slope"], add = TRUE)
+"dem_lisbon_raster"
+
+#' Road segments in Lisbon
+#'
+#' A dataset representing road segments in Lisbon, with X, Y and Z (elevation) dimensions for each coordinate.
+#'
+#' @format And object of class `sf`
+#' @source Produced by ESRI's [3D Analyst extension](https://pro.arcgis.com/en/pro-app/help/analysis/3d-analyst/get-started-with-3d-analyst-in-pro.htm)
+#' @examples
+#' names(lisbon_road_segments)
+#' plot(lisbon_road_segments["Avg_Slope"])
+"lisbon_road_segments"
--- data-raw/dem_lisbon.R
+++ data-raw/dem_lisbon.R
@@ -8,6 +8,27 @@ lisbon_road_segments_esri = sf::read_sf("~/wip/pctLisbon-data/Shapefiles/route_s
 lisbon_1km_projected = sf::st_transform(lisbon_1km, sf::st_crs(lisbon_road_segments_esri))
 lisbon_road_segments = sf::st_intersection(lisbon_road_segments_esri, lisbon_1km_projected)
 plot(lisbon_road_segments["Avg_Slope"])
+usethis::use_data(lisbon_road_segments)
 
+download.file("https://github.com/geocompr/d/releases/download/1/r1.zip", "r1.zip")
+unzip("r1.zip")dem_lisbon_raster = raster::raster("r1")
+dem_lisbon_raster = raster::crop(dem_lisbon_raster, lisbon_1km_projected)
+dem_lisbon_raster = raster::trim(dem_lisbon_raster)
+raster::crs(dem_lisbon_raster) # no CRS
+raster::plot(dem_lisbon_raster)
+usethis::use_data(dem_lisbon_raster, overwrite = TRUE)
+file.size("data/dem_lisbon_raster.rda") / 1e6 # tiny dataset
+unlink("r1", recursive = TRUE)
+unlink("r1.zip")
 
-usethis::use_data(dem_lisbon, overwrite = TRUE)
+# saving alternative formats
+dir.create("inst")
+# raster::writeRaster(dem_lisbon_raster, filename = "dem_lisbon_raster.tif", options("COMPRESS=DEFLATE"), overwrite = TRUE, format = ".tif")
+raster::writeRaster(dem_lisbon_raster, "inst/dem_lisbon_raster.asc", overwrite = TRUE)
+file.size("inst/dem_lisbon_raster.asc") / 1e6 # large, so deleted
+
+# With terra
+lisbon_road_segments_terra = terra::vect("~/wip/pctLisbon-data/Shapefiles/route_segments_gradient2_Zelevation.gpkg")
+dem_lisbon_terra = terra::rast("r1/hdr.adf")
+dem_lisbon_terra = terra::crop(dem_lisbon_terra, terra::ext(lisbon_road_segments_terra))
+
