98fcd53a28cbbc7e35bf4ac943245eef36dc4b39
Robin Lovelace <rob00x@gmail.com>
2021-02-02 21:12:46 +0000

    Fix tests

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,6 +1,8 @@
-test_that("slope_* functions work", {
+test_that("functions to get elevations from sf objects work", {
+  if(nchar(Sys.getenv("MAPBOX")) < 8)
+    skip(message = "Skipping test, MAPBOX token in .Renviron needed")
   r = cyclestreets_route
   e = elevations_get(r)
-  skip_if(nchar(Sys.getenv("MAPBOX") < 8))
   expect_equal(class(e)[1], "RasterLayer")
 })
+
