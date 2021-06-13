8f89c3d3e2005304db186feadcdba809e79e98ae
temospena <temospena@gmail.com>
2021-06-13 12:57:39 +0100

    replace e for elevations (values) and e for dem (raster)

--- R/slopes.R
+++ R/slopes.R
@@ -20,39 +20,39 @@
 #'
 #' @param x Vector of locations
 #' @param d Vector of distances between points
-#' @param e Elevations in same units as x (assumed to be metres)
+#' @param elevations Elevations in same units as x (assumed to be metres)
 #' @export
 #' @examples
 #' x = c(0, 2, 3, 4, 5, 9)
-#' e = c(1, 2, 2, 4, 3, 1) / 10
-#' slope_vector(x, e)
+#' elevations = c(1, 2, 2, 4, 3, 1) / 10
+#' slope_vector(x, elevations)
 #' m = sf::st_coordinates(lisbon_road_segment)
 #' d = sequential_dist(m, lonlat = FALSE)
-#' e = elevation_extract(m, dem_lisbon_raster)
-#' slope_distance(d, e)
-#' slope_distance_mean(d, e)
-#' slope_distance_weighted(d, e)
-slope_vector = function(x, e) {
+#' elevations = elevation_extract(m, dem_lisbon_raster)
+#' slope_distance(d, elevations)
+#' slope_distance_mean(d, elevations)
+#' slope_distance_weighted(d, elevations)
+slope_vector = function(x, elevations) {
   d = diff(x)
-  e_change = diff(e)
+  e_change = diff(elevations)
   e_change / d
 }
 #' @rdname slope_vector
 #' @export
-slope_distance = function(d, e) {
-  e_change = diff(e)
+slope_distance = function(d, elevations) {
+  e_change = diff(elevations)
   e_change / d
 }
 #' @rdname slope_vector
 #' @export
-slope_distance_mean = function(d, e) {
-  e_change = diff(e)
+slope_distance_mean = function(d, elevations) {
+  e_change = diff(elevations)
   mean(abs(e_change) / d)
 }
 #' @rdname slope_vector
 #' @export
-slope_distance_weighted = function(d, e) {
-  e_change = diff(e)
+slope_distance_weighted = function(d, elevations) {
+  e_change = diff(elevations)
   stats::weighted.mean(abs(e_change) / d, d)
 }
 #' Calculate the gradient of line segments from a 3D matrix of coordinates
@@ -74,16 +74,16 @@ slope_distance_weighted = function(d, e) {
 #' points(x[-length(x)], gxy, col = "blue")
 #' title("Distance (in x coordinates) elevation profile",
 #'   sub = "Points show calculated gradients of subsequent lines")
-slope_matrix = function(m, e = m[, 3], lonlat = TRUE) {
+slope_matrix = function(m, elevations = m[, 3], lonlat = TRUE) {
   d = sequential_dist(m, lonlat = lonlat)
-  e_change = diff(e)
+  e_change = diff(elevations)
   g = e_change / d
   g
 }
 #' @rdname slope_matrix
 #' @export
-slope_matrix_weighted = function(m, e = m[, 3], lonlat = TRUE) {
-  g1 = slope_matrix(m, e = e, lonlat = lonlat)
+slope_matrix_weighted = function(m, elevations = m[, 3], lonlat = TRUE) {
+  g1 = slope_matrix(m, elevations = elevations, lonlat = lonlat)
   d = sequential_dist(m = m, lonlat = lonlat)
   stats::weighted.mean(abs(g1), d, na.rm = TRUE)
 }
@@ -137,7 +137,7 @@ slope_matrices = function(m_xyz_split, fun = slope_matrix_weighted, ...) {
 #' @inheritParams elevation_extract
 #' @param routes Routes, the gradients of which are to be calculated.
 #'   The object must be of class `sf` with `LINESTRING` geometries.
-#' @param e Raster overlapping with `routes` and values representing elevations
+#' @param dem Raster overlapping with `routes` and values representing elevations
 #' @param method The method of estimating elevation at points,
 #'   passed to the `extract` function for extracting values from raster
 #'   datasets. Default: `"bilinear"`.
@@ -147,23 +147,23 @@ slope_matrices = function(m_xyz_split, fun = slope_matrix_weighted, ...) {
 #' @export
 #' @examples
 #' routes = lisbon_road_segments[1:3, ]
-#' e = dem_lisbon_raster
-#' (s = slope_raster(routes, e))
+#' dem = dem_lisbon_raster
+#' (s = slope_raster(routes, dem))
 #' cor(routes$Avg_Slope, s)
 slope_raster = function(
  routes,
-  e = NULL,
+  dem = NULL,
   lonlat = sf::st_is_longlat(routes),
   method = "bilinear",
   fun = slope_matrix_weighted,
-  terra = has_terra() && methods::is(e, "SpatRaster")
+  terra = has_terra() && methods::is(dem, "SpatRaster")
   ) {
   stop_is_not_linestring(routes)
   routes = sf::st_geometry(routes)
   # todo: split out this bit into slope_xyz function
   m = sf::st_coordinates(routes)
   # colnames(m)
-  z = elevation_extract(m, e, method = method, terra = terra)
+  z = elevation_extract(m, dem, method = method, terra = terra)
   m_xyz_df = data.frame(x = m[, "X"], y = m[, "Y"], z = z, L1 = m[, "L1"])
   res = slope_xyz(m_xyz_df, fun = fun, lonlat = lonlat)
   res
@@ -202,15 +202,15 @@ slope_xyz = function(r_xyz, fun = slope_matrix_weighted, lonlat = NULL) {
 #'
 #' @param terra Should the `terra` package be used?
 #' `TRUE` by default if the package is installed *and*
-#' if `e` is of class `SpatRast`
+#' if `dem` is of class `SpatRast`
 #' @inheritParams slope_raster
 #' @inheritParams slope_matrix
 #' @export
 #' @examples
 #' m = sf::st_coordinates(lisbon_road_segments[1, ])
-#' e = dem_lisbon_raster
-#' elevation_extract(m, e)
-#' elevation_extract(m, e, method = "simple")
+#' dem = dem_lisbon_raster
+#' elevation_extract(m, dem)
+#' elevation_extract(m, dem, method = "simple")
 #' # uncomment the following lines to test with terra (experimental)
 #' # u = paste0("https://github.com/ITSLeeds/slopes/",
 #' #    "releases/download/0.0.0/dem_lisbon.tif" )
@@ -218,14 +218,14 @@ slope_xyz = function(r_xyz, fun = slope_matrix_weighted, lonlat = NULL) {
 #' # et = terra::rast("dem_lisbon.tif")
 #' # elevation_extract(m, et)
 elevation_extract = function(m,
-                             e,
+                             dem,
                              method = "bilinear",
-                             terra = has_terra() && methods::is(e, "SpatRaster")
+                             terra = has_terra() && methods::is(dem, "SpatRaster")
                              ) {
   if(terra) {
-    res = terra::extract(e, m[, 1:2], method = method)[[1]]
+    res = terra::extract(dem, m[, 1:2], method = method)[[1]]
   } else {
-    res = raster::extract(e, m[, 1:2], method = method)
+    res = raster::extract(dem, m[, 1:2], method = method)
   }
   # unlist(res)
   res
@@ -237,8 +237,8 @@ elevation_extract = function(m,
 #' @export
 #' @examples
 #' routes = lisbon_road_segments[204, ]
-#' e = dem_lisbon_raster
-#' (r3d = slope_3d(routes, e))
+#' dem = dem_lisbon_raster
+#' (r3d = slope_3d(routes, dem))
 #' sf::st_z_range(routes)
 #' sf::st_z_range(r3d)
 #' plot(sf::st_coordinates(r3d)[, 3])
@@ -247,22 +247,22 @@ elevation_extract = function(m,
 #' # plot_slope(r3d_get)
 slope_3d = function(
  routes,
-  e = NULL,
+  dem = NULL,
   method = "bilinear",
-  terra = has_terra() && methods::is(e, "SpatRaster")
+  terra = has_terra() && methods::is(dem, "SpatRaster")
   ) {
-  if(is.null(e)) {
-    e = elevations_get(routes)
+  if(is.null(dem)) {
+    dem = elevations_get(routes)
     r_original = routes # create copy to deal with projection issues
-    routes = sf::st_transform(routes, raster::crs(e))
+    routes = sf::st_transform(routes, raster::crs(dem))
     suppressWarnings({sf::st_crs(routes) = sf::st_crs(r_original)})
     m = sf::st_coordinates(routes)
     mo = sf::st_coordinates(r_original)
-    z = as.numeric(elevation_extract(m, e, method = method, terra = terra))
+    z = as.numeric(elevation_extract(m, dem, method = method, terra = terra))
     m_xyz = cbind(mo[, 1:2], z)
   } else {
     m = sf::st_coordinates(routes)
-    z = as.numeric(elevation_extract(m, e, method = method, terra = terra))
+    z = as.numeric(elevation_extract(m, dem, method = method, terra = terra))
     m_xyz = cbind(m[, 1:2], z)
   }
   n = nrow(routes)
