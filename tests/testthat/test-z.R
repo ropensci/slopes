1ee5992c700dab7ddbcf8a5e6096fe6b5b8916aa
Robin Lovelace <rob00x@gmail.com>
2021-09-10 18:22:24 +0100

    Update test-z.R

--- tests/testthat/test-z.R
+++ tests/testthat/test-z.R
@@ -6,8 +6,11 @@ test_that("Functions on Z values work", {
   expect_error(z_start(x))
   expect_error(z_end(x))
   expect_error(z_mean(x))
-  expect_error(z_max(x))
   expect_error(z_min(x))
+  expect_error(z_max(x))
+  expect_error(z_elevation_change_start_end(x))
+  expect_error(z_direction(x))
+  expect_error(z_cumulative_difference(x))
   # x_slope =
   # expect_is()
 })
